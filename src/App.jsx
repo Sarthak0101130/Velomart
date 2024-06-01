0b61d3e7f642adab2e4b4587563d9997a0650544
Vrisha Garg <vrishagarg0511@gmail.com>
2024-06-02 00:21:35 +0530

    issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -11,3 +11,36 @@ import Testimonials from "./components/Testimonials/Testimonials";
 import Footer from "./components/Footer/Footer";
 import Popup from "./components/Popup/Popup";
 
+const App = () => {
+    const [orderPopup, setOrderPopup] = React.useState(false);
+  
+    const handleOrderPopup = () => {
+      setOrderPopup(!orderPopup);
+    };
+    React.useEffect(() => {
+      AOS.init({
+        offset: 100,
+        duration: 800,
+        easing: "ease-in-sine",
+        delay: 100,
+      });
+      AOS.refresh();
+    }, []);
+  
+    return (
+      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <Navbar handleOrderPopup={handleOrderPopup} />
+        <Hero handleOrderPopup={handleOrderPopup} />
+        <Products />
+        <TopProducts handleOrderPopup={handleOrderPopup} />
+        <Banner />
+        <Subscribe />
+        <Products />
+        <Testimonials />
+        <Footer />
+        <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+      </div>
+    );
+  };
+
+  export default App;
